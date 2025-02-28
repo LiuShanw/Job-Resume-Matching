b4d8485b59b3a6c30e9f5df6dd8b7c616b1bb3b8
Moon <104545661+LiuShanw@users.noreply.github.com>
2025-02-28 10:22:13 +0000

    i guess no error now

--- source/db_helpers/db_connection.py
+++ source/db_helpers/db_connection.py
@@ -1,8 +1,11 @@
 import motor.motor_asyncio
-
+from motor.motor_asyncio import AsyncIOMotorDatabase
 
 MONGO_DETAILS = "mongodb://matchingproject:matchingproject@mongodb"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
 database = client['matchingdb']
+
+def get_database() -> AsyncIOMotorDatabase:
+    return database
